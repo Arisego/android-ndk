f3e076b4b15f94c5917df119a7ae8b7963b488ad
Naru <3301325+Arisego@users.noreply.github.com>
2018-08-22 16:45:30 +0800

    Mirror UE4 socket code
    It works

--- hello-jni/app/src/main/cpp/hello-jni.c
+++ hello-jni/app/src/main/cpp/hello-jni.c
@@ -17,6 +17,26 @@
 #include <string.h>
 #include <jni.h>
 
+#include <stdint.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+#include <stdio.h>
+#include <errno.h>
+
+#include<android/log.h>
+
+#define TAG "myDemo-jni" // 这个是自定义的LOG的标识
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,TAG ,__VA_ARGS__) // 定义LOGD类型
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,TAG ,__VA_ARGS__) // 定义LOGI类型
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,TAG ,__VA_ARGS__) // 定义LOGW类型
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__) // 定义LOGE类型
+#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL,TAG ,__VA_ARGS__) // 定义LOGF类型
+
+extern int errno;
+
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
  * file located at:
@@ -59,5 +79,36 @@ Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
 #define ABI "unknown"
 #endif
 
-    return (*env)->NewStringUTF(env, "Hello from JNI !  Compiled with ABI " ABI ".");
+    jstring ts_ret;
+    char buffer [50];
+    sprintf (buffer, "Everything is right %d", 2);
+
+    int TempSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if(TempSocket)
+    {
+        struct ifreq  IfReqs[8];
+        struct ifconf IfConfig;
+
+        memset(&IfConfig, 0, sizeof(IfConfig));
+        IfConfig.ifc_ifcu.ifcu_req = IfReqs;
+        IfConfig.ifc_len = sizeof(IfReqs);
+
+        int Result = ioctl(TempSocket, SIOCGIFCONF, &IfConfig);
+        if(Result == 0)
+        {
+
+        }
+        else
+        {
+            int ErrNo = errno;
+            sprintf(buffer, "Ioctl error %d||%d||%s", Result, errno, strerror(ErrNo));
+        }
+    }
+    else
+    {
+        sprintf(buffer, "Socket create failed");
+    }
+
+    ts_ret = (*env)->NewStringUTF(env, buffer);
+    return ts_ret;
 }
